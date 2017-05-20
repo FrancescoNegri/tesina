6a396b76704e8d8fe156a729e25114b48585bc51
Francesco Negri <francesco.negri.98@gmail.com>
2017-05-20 15:34:59 +0200

    fix

--- src/states/menu.js
+++ src/states/menu.js
@@ -2,11 +2,9 @@ var menuState = {
     create: function () {
 
         var nameLabel = game.add.text(80, 80, 'My first game - TESINA', { font: '25px Arial', fill: '#FFFFFF' });
-
         var startLabel = game.add.text(80, game.world.height - 80, 'press the W key', { font: '25px Arial', fill: '#FFFFFF' });
 
         var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
-
         wkey.onDown.addOnce(this.start, this);
     },
 
--- src/states/test.js
+++ src/states/test.js
@@ -1,9 +1,8 @@
 var testState = {
     create: function() {
-        var nameLabel = game.add.text(80, 80, 'Testing', {font: '25px Arial', fill: '#FFFFFF'});
+        var nameLabel = game.add.text(80, 80, 'Testing (premere W)', {font: '25px Arial', fill: '#FFFFFF'});
 
         var wkey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
         wkey.onDown.addOnce(this.start, this);
     },
 
