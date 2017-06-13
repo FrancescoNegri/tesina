74c675a5d720c971b4b82b9fc5fa401f7d55480d
Francesco Negri <francesco.negri.98@gmail.com>
2017-06-13 15:55:34 +0200

    level definitivo

--- src/config.js
+++ src/config.js
@@ -8,8 +8,6 @@ var playCutscene = true;
 //TODO:
 /* 
 - Fare cutscene finale e apertura chest + vittoria!
-- Creare level definitivo
-- Disporre le cutscene e gli oggetti in modo sensato e giusto
 - Calibrare correttamente i tempi dei tween
 - Aggiungere supporto completo XBOX Gamepad
 - Rivedere musica intro
